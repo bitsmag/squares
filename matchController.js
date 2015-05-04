d2d53a1725e9cbbb4bb46169bf99a5dbd9041e80
bitsmag <keinath.s@googlemail.com>
2015-05-04 14:23:54 +0200

    moved position conflict handling to getNewPlayerPosition and added tags file

--- matchController.js
+++ matchController.js
@@ -54,7 +54,7 @@ MatchController.prototype.matchTicker = function(){
       clearInterval(tickerInterval);
     }
     else{
-      that.calculateBoard();
+      that.updateBoard();
       matchSockets.sendUpdateBoardEvent(that.match.id);
     }
   }
@@ -62,81 +62,146 @@ MatchController.prototype.matchTicker = function(){
   var tickerInterval = setInterval(tick, 500);
 }
 
-MatchController.prototype.calculateBoard = function(){
-  // Current Positions
-  var orangeCurrent = this.match.getPlayerByColor('orange').position;
-  var blueCurrent = this.match.getPlayerByColor('blue').position;
-  var greenCurrent = this.match.getPlayerByColor('green').position;
-  var redCurrent = this.match.getPlayerByColor('red').position;
-
-  // Positions players want to move to
-  var orangeFuture = this.calculateStep(orangeCurrent, this.match.getPlayerByColor('orange').activeDirection);
-  var blueFuture = this.calculateStep(blueCurrent, this.match.getPlayerByColor('blue').activeDirection);
-  var greenFuture = this.calculateStep(greenCurrent, this.match.getPlayerByColor('green').activeDirection);
-  var redFuture = this.calculateStep(redCurrent, this.match.getPlayerByColor('red').activeDirection);
-
-  // Decide randomly on conflicts
-  if(blueFuture === orangeFuture){
-    if(Math.floor(Math.random()*2)===0){
-      blueFuture = blueCurrent;
-    }
-    else{
-      orangeFuture = orangeCurrent;
-    }
-  }
-  if(blueFuture === redFuture){
-    if(Math.floor(Math.random()*2)===0){
-      blueFuture = blueCurrent;
-    }
-    else{
-      redFuture = redCurrent;
-    }
-  }
-  if(blueFuture === greenFuture){
-    if(Math.floor(Math.random()*2)===0){
-      blueFuture = blueCurrent;
-    }
-    else{
-      greenFuture = greenCurrent;
-    }
-  }
-  if(redFuture === greenFuture){
-    if(Math.floor(Math.random()*2)===0){
-      redFuture = redCurrent;
-    }
-    else{
-      greenFuture = greenCurrent;
-    }
-  }
-  if(redFuture === orangeFuture){
-    if(Math.floor(Math.random()*2)===0){
-      redFuture = redCurrent;
-    }
-    else{
-      orangeFuture = orangeCurrent;
-    }
-  }
-  if(greenFuture === orangeFuture){
-    if(Math.floor(Math.random()*2)===0){
-      greenFuture = greenCurrent;
-    }
-    else{
-      orangeFuture = orangeCurrent;
-    }
-  }
+MatchController.prototype.updateBoard = function(){
+  var positions = this.getNewPlayerPositions();
 
   // Set new positions on players
-  this.match.getPlayerByColor('orange').position = orangeFuture;
-  this.match.getPlayerByColor('blue').position = blueFuture;
-  this.match.getPlayerByColor('green').position = greenFuture;
-  this.match.getPlayerByColor('red').position = redFuture;
+  this.match.getPlayerByColor('blue').position = positions.blue;
+  this.match.getPlayerByColor('orange').position = positions.orange;
+  this.match.getPlayerByColor('green').position = positions.green;
+  this.match.getPlayerByColor('red').position = positions.red;
 
   // Set color on board
   // TODO: Don't apply color if player is not moving
-  this.match.board.getSquare(orangeFuture).color = 'orange';
-  this.match.board.getSquare(blueFuture).color = 'blue';
-  this.match.board.getSquare(greenFuture).color = 'green';
-  this.match.board.getSquare(redFuture).color = 'red';
+  this.match.board.getSquare(positions.blue).color = 'blue';
+  this.match.board.getSquare(positions.orange).color = 'orange';
+  this.match.board.getSquare(positions.green).color = 'green';
+  this.match.board.getSquare(positions.red).color = 'red';
+}
+
+MatchController.prototype.getNewPlayerPositions = function(){ // TODO: Make this function a little bit nicer...
+  // Current Positions
+  var blueCurrent = this.match.getPlayerByColor('blue').position;
+  var orangeCurrent = this.match.getPlayerByColor('orange').position;
+  var greenCurrent = this.match.getPlayerByColor('green').position;
+  var redCurrent = this.match.getPlayerByColor('red').position;
+
+  // Positions players wants to move to
+  var blueFuture = this.calculateStep(blueCurrent, this.match.getPlayerByColor('blue').activeDirection);
+  var orangeFuture = this.calculateStep(orangeCurrent, this.match.getPlayerByColor('orange').activeDirection);
+  var greenFuture = this.calculateStep(greenCurrent, this.match.getPlayerByColor('green').activeDirection);
+  var redFuture = this.calculateStep(redCurrent, this.match.getPlayerByColor('red').activeDirection);
+
+  // If a player stands still it has priority on conflict calculation
+  var blueP, orangeP, greenP, redP = false;
+  if(blueCurrent === blueFuture) blueP = true;
+  if(orangeCurrent === orangeFuture) orangeP = true;
+  if(greenCurrent === greenFuture) greenP = true;
+  if(redCurrent === redFuture) redP = true;
+
+  // Deal with conflicts
+  if(blueFuture === orangeFuture){
+    if(!blueP && !orangeP){ // If noone has priority decide randomly
+      if(Math.floor(Math.random()*2)===0){
+        blueFuture = blueCurrent;
+      }
+      else{
+        orangeFuture = orangeCurrent;
+      }
+    }
+    else if(blueP){ // If blue has priority orange cant move
+      orangeFuture = orangeCurrent;
+    }
+    else if(orangeP){ // If orange has priority blue cant move
+      blueFuture = blueCurrent;
+    }
+  }
+  if(blueFuture === redFuture){
+    if(!blueP && !redP){
+      if(Math.floor(Math.random()*2)===0){
+        blueFuture = blueCurrent;
+      }
+      else{
+        redFuture = redCurrent;
+      }
+    }
+    else if(blueP){
+      redFuture = redCurrent;
+    }
+    else if(redP){
+      blueFuture = blueCurrent;
+    }
+  }
+  if(blueFuture === greenFuture){
+    if(!blueP && !greenP){
+      if(Math.floor(Math.random()*2)===0){
+        blueFuture = blueCurrent;
+      }
+      else{
+        greenFuture = greenCurrent;
+      }
+    }
+    else if(blueP){
+      greenFuture = greenCurrent;
+    }
+    else if(greenP){
+      blueFuture = blueCurrent;
+    }
+  }
+  if(redFuture === greenFuture){
+    if(!redP && ! greenP){
+      if(Math.floor(Math.random()*2)===0){
+        redFuture = redCurrent;
+      }
+      else{
+        greenFuture = greenCurrent;
+      }
+    }
+    else if(redP){
+      greenFuture = greenCurrent;
+    }
+    else if(greenP){
+      redFuture = redCurrent;
+    }
+  }
+  if(redFuture === orangeFuture){
+    if(!redP && !orangeP){
+      if(Math.floor(Math.random()*2)===0){
+        redFuture = redCurrent;
+      }
+      else{
+        orangeFuture = orangeCurrent;
+      }
+    }
+    else if(redP){
+      orangeFuture = orangeCurrent;
+    }
+    else if(orangeP){
+      redFuture = redCurrent;
+    }
+  }
+  if(greenFuture === orangeFuture){
+    if(!greenP && !orangeP){
+      if(Math.floor(Math.random()*2)===0){
+        greenFuture = greenCurrent;
+      }
+      else{
+        orangeFuture = orangeCurrent;
+      }
+    }
+    else if(greenP){
+      orangeFuture = orangeCurrent;
+    }
+    else if(orangeP){
+      greenFuture = greenCurrent;
+    }
+  }
+
+  var positions = {blue: blueFuture,
+    orange: orangeFuture,
+    green: greenFuture,
+    red: redFuture}
+  return positions;
 }
 
 MatchController.prototype.calculateStep = function(currentPosition, activeDirection){
